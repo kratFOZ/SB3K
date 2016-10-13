63b64f4036c06170391bb5d332ccc6ee731fa441
Sadi Muhammad Wali <sadiwali@users.noreply.github.com>
2016-10-13 16:44:20 -0400

    Add files via upload
    This is the C# code + GUI used to control, and receive radar data from robot.

---
+++ ROBO_TEJ _C#/ROBO_TEJ/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace ROBO_TEJ
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrMain = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEngine = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.btnW = new System.Windows.Forms.Button();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnS = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.spMain = new System.IO.Ports.SerialPort(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tmrMain
+            // 
+            this.tmrMain.Interval = 2;
+            this.tmrMain.Tick += new System.EventHandler(this.tmrMain_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(727, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(31, 549);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "000";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnEngine
+            // 
+            this.btnEngine.Location = new System.Drawing.Point(131, 66);
+            this.btnEngine.Name = "btnEngine";
+            this.btnEngine.Size = new System.Drawing.Size(95, 23);
+            this.btnEngine.TabIndex = 2;
+            this.btnEngine.Text = "Engine (s:OFF)";
+            this.btnEngine.UseVisualStyleBackColor = true;
+            this.btnEngine.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(13, 66);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(56, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.ForeColor = System.Drawing.Color.Lime;
+            this.lblSpeed.Location = new System.Drawing.Point(52, 16);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(95, 27);
+            this.lblSpeed.TabIndex = 4;
+            this.lblSpeed.Text = "000";
+            this.lblSpeed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnW
+            // 
+            this.btnW.BackColor = System.Drawing.Color.White;
+            this.btnW.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnW.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnW.Location = new System.Drawing.Point(75, 46);
+            this.btnW.Name = "btnW";
+            this.btnW.Size = new System.Drawing.Size(50, 50);
+            this.btnW.TabIndex = 5;
+            this.btnW.Text = "W";
+            this.btnW.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnW.UseVisualStyleBackColor = false;
+            // 
+            // btnA
+            // 
+            this.btnA.BackColor = System.Drawing.Color.White;
+            this.btnA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnA.Location = new System.Drawing.Point(26, 95);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(50, 50);
+            this.btnA.TabIndex = 5;
+            this.btnA.Text = "A";
+            this.btnA.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnA.UseVisualStyleBackColor = false;
+            // 
+            // btnS
+            // 
+            this.btnS.BackColor = System.Drawing.Color.White;
+            this.btnS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnS.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnS.Location = new System.Drawing.Point(75, 144);
+            this.btnS.Name = "btnS";
+            this.btnS.Size = new System.Drawing.Size(50, 50);
+            this.btnS.TabIndex = 5;
+            this.btnS.Text = "S";
+            this.btnS.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnS.UseVisualStyleBackColor = false;
+            // 
+            // btnD
+            // 
+            this.btnD.BackColor = System.Drawing.Color.White;
+            this.btnD.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnD.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnD.Location = new System.Drawing.Point(124, 95);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(50, 50);
+            this.btnD.TabIndex = 5;
+            this.btnD.Text = "D";
+            this.btnD.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnD.UseVisualStyleBackColor = false;
+            // 
+            // spMain
+            // 
+            this.spMain.BaudRate = 115200;
+            this.spMain.PortName = "COM34";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.Lime;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(5, 392);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(716, 169);
+            this.listBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(2, 362);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 27);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "LOG";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(761, 567);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnS);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.btnW);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnEngine);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Name = "frmMain";
+            this.Text = "CONTROL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrMain;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEngine;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.Button btnW;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnS;
+        private System.Windows.Forms.Button btnD;
+        private System.IO.Ports.SerialPort spMain;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ROBO_TEJ _C#/ROBO_TEJ/Form1.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.IO.Ports;
+
+namespace ROBO_TEJ
+{
+
+
+    public partial class frmMain : Form
+    {
+        //Console construct
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        int y,yMax,xMax;
+
+        //public controllers
+        int ctrl_powerLevel;
+        int ctrl_powerLeft, ctrl_powerRight;
+        Int16 ctrl_engineOn = 0;
+        Int16 ctrl_dirMod = 1;
+
+        bool serialEnable = true;
+
+
+        bool[,] envArr = new bool[20, 20];
+
+
+        private bool f, b, l, r;
+
+        int ca = 0;
+        int[] carr = new int[20]; 
+
+
+
+       
+        public frmMain()
+        {
+        
+            InitializeComponent();
+
+            spMain.PortName = "COM34";
+            spMain.BaudRate = 38400;
+
+            if (serialEnable)
+            {
+                try
+                {
+                    spMain.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                spMain.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(DataReceived);
+            }
+        }
+
+
+
+
+        private void DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+              
+                SerialPort spl = (SerialPort)sender;        
+                string s = spl.ReadLine();
+
+                Console.WriteLine(s);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            xMax = envArr.GetLength(1) - 1 / 2;
+            yMax = envArr.GetLength(0) - 1 / 2;
+            AllocConsole();
+            stopEngine();
+            this.Location = new Point(100, 200);
+            this.KeyPreview = true;
+            this.KeyDown += frmMain_KeyDown;
+            this.KeyUp += frmMain_KeyUp;
+            //RecSig();
+        }
+
+
+
+       
+        //powerLevel modification done here
+        void frmMain_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                {
+                    if (!b)
+                    {
+                        btnW.BackColor = Color.Yellow;
+                        f = true;
+                    }
+                    break;
+                }
+                case Keys.A:
+                {
+                    if (!r)
+                    {
+                        btnA.BackColor = Color.Yellow;
+                        l = true;
+                    }
+                    break;
+                        
+                }
+                case Keys.S:
+                {
+                    if (!f)
+                    {
+                        btnS.BackColor = Color.Yellow;
+                        b = true;
+                            
+                    }
+                    break;
+                }
+                case Keys.D:
+                {
+                    if (!l)
+                    {
+                        btnD.BackColor = Color.Yellow;
+                        r = true;
+                    }
+                    break;
+                }
+            }
+
+        }
+
+        void frmMain_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    {
+                        btnW.BackColor = Color.White;
+                        f = false;
+                        break;
+                    }
+                case Keys.A:
+                    {
+                        btnA.BackColor = Color.White;
+                        l = false;
+                        break;
+
+                    }
+                case Keys.S:
+                    {
+                        btnS.BackColor = Color.White;
+                        b = false;
+                        break;
+                    }
+                case Keys.D:
+                    {
+                        btnD.BackColor = Color.White;
+                        r = false;
+                        break;
+                    }
+            }
+
+
+            ctrl_powerLeft = 0;ctrl_powerRight = 0;
+
+        }
+
+
+        private void RecSig()
+        {
+
+        }
+
+        private void SendSig()
+        {
+            
+            /*string format:
+             *0 - direction modifier (0 = forward, 1 = backward, 2 = left, 3 = right)
+             *1 - left power (0-9)
+             *2 - right power (0-9)
+             */
+            string s = "" + ctrl_dirMod + ctrl_powerLeft.ToString("0") + ctrl_powerRight.ToString("0");
+            if (serialEnable)
+            {      
+                spMain.WriteLine(s);
+                Console.WriteLine(s);
+            }
+        }
+
+
+
+
+        //controls mouse power
+        private void tmrMain_Tick(object sender, EventArgs e)
+        {
+            y = Screen.PrimaryScreen.Bounds.Height/2-Cursor.Position.Y;
+
+            if (y > panel1.Height/2)
+                y = panel1.Height/2-5;
+            else if (y < -panel1.Height/2)
+                y = -panel1.Height/2+5;
+
+            ctrl_powerLevel = (int)Math.Round((((double)y/(double)panel1.Height*2)*100.0) / 10.0) * 10;
+            if (ctrl_powerLevel >= 100)
+                ctrl_powerLevel = 90;
+
+            if (ctrl_powerLevel > 0)
+                lblSpeed.Text = "Current speed:" + Math.Abs(ctrl_powerLevel) + '%';
+            else
+            {
+                ctrl_powerLevel = 0;
+                lblSpeed.Text = "Stopped";
+            }
+
+
+            label1.Top = (panel1.Height/2-y);
+            label1.Text = ctrl_powerLevel.ToString();
+            ctrl_powerLevel = ctrl_powerLevel / 10;
+            ctrl_powerLevel = Math.Abs(ctrl_powerLevel);
+
+            //forward or back
+            if ((f || b) && !l && !r)
+            {
+                if (b)
+                    ctrl_dirMod = 2;
+                else
+                    ctrl_dirMod = 1;
+                ctrl_powerLeft = ctrl_powerLevel;
+                ctrl_powerRight = ctrl_powerLeft;
+            }
+            //just left
+            else if ((l || r) && !f && !b)
+            {
+                if (l)
+                    ctrl_dirMod = 3;
+                else
+                    ctrl_dirMod = 4;
+                ctrl_powerLeft = ctrl_powerLevel;
+                ctrl_powerRight = ctrl_powerLeft;
+            }
+            else if ((f || b) && (l || r))
+            {
+                if ((f || b) && l)
+                {
+                    if (f)
+                        ctrl_dirMod = 1;
+                    else
+                        ctrl_dirMod = 2;
+
+                    ctrl_powerRight = ctrl_powerLevel;
+                    ctrl_powerLeft = (int)Math.Round((double)ctrl_powerRight / 2);
+                }
+                else if ((f || b) && r)
+                {
+                    if (f)
+                        ctrl_dirMod = 1;
+                    else
+                        ctrl_dirMod = 2;
+                    ctrl_powerLeft = ctrl_powerLevel;
+                    ctrl_powerRight = (int)Math.Round((double)ctrl_powerLeft / 2);
+                }
+
+            }
+
+            SendSig();
+        }
+
+        void startEngine()
+        {    
+            Console.Clear();
+            tmrMain.Enabled = true;
+           // tmrControl.Enabled =true;
+            ctrl_engineOn = 1;
+            btnEngine.Text = "Engine (is:ON)";
+            panel1.Enabled = true;
+            panel1.BackColor = Color.Yellow;
+            label1.Visible = true;
+            ctrl_powerLevel = 0;            
+        }
+
+        void stopEngine()
+        {
+            ctrl_engineOn = 0;
+            SendSig();
+            tmrMain.Enabled = false;
+           // tmrControl.Enabled = false;
+            ctrl_engineOn = 0;
+            btnEngine.Text = "Engine (is:OFF)";
+            panel1.Enabled = false;            
+            panel1.BackColor = Color.DimGray;
+            label1.Visible = false;
+            ctrl_powerLevel = 0;
+            lblSpeed.Text = "ENGINE OFF";
+        }
+
+        //engine starter toggle
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ctrl_engineOn == 1)
+                stopEngine();
+            else
+                startEngine();
+        }
+
+        //controls keyboard direction
+        private void tmrKeyb_Tick(object sender, EventArgs e)
+        {
+            //forward or back
+            if ((f || b) && !l && !r)
+            {
+                if(b)
+                    ctrl_dirMod = 2;
+                else
+                    ctrl_dirMod = 1;
+                ctrl_powerLeft = ctrl_powerLevel;
+                ctrl_powerRight = ctrl_powerLeft ;
+            }
+            //just left
+            else if ((l || r) && !f && !b)
+            {
+                if (l)
+                    ctrl_dirMod = 3;
+                else
+                    ctrl_dirMod = 4;
+                ctrl_powerLeft = ctrl_powerLevel;
+                ctrl_powerRight = ctrl_powerLeft;
+            }
+            else if ((f || b) && (l || r))
+            {
+                if ((f || b) && l)
+                {
+                    if (f)
+                        ctrl_dirMod = 1;
+                    else
+                        ctrl_dirMod = 2;
+
+                    ctrl_powerRight = ctrl_powerLevel;
+                    ctrl_powerLeft = (int)Math.Round((double)ctrl_powerRight/2);
+                }
+                else if ((f || b) && r)
+                {
+                    if (f)
+                        ctrl_dirMod = 1;
+                    else
+                        ctrl_dirMod = 2;
+                    ctrl_powerLeft = ctrl_powerLevel;
+                    ctrl_powerRight = (int)Math.Round((double)ctrl_powerLeft / 2);
+                }
+
+            }
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
